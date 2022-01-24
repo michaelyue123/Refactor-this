e298287b599a322cf8bb632f9e18ba39fc9ade5d
s3796237 <michaelyue123@gmail.com>
2022-01-24 17:54:22 +1100

    fix project warnings + upgrade deprecated .net version

---
+++ Models/Products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactorThis.Models
+{
+    public class Products
+    {
+        public List<Product> Items { get; private set; }
+
+        public Products()
+        {
+            LoadProducts(null);
+        }
+
+        public Products(string name)
+        {
+            LoadProducts($"where lower(name) like '%{name.ToLower()}%'");
+        }
+
+        private void LoadProducts(string where)
+        {
+            Items = new List<Product>();
+            var conn = Helpers.NewConnection();
+            conn.Open();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = $"select id from Products {where}";
+
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var id = Guid.Parse(rdr.GetString(0));
+                Items.Add(new Product(id));
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace RefactorThis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
